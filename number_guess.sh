2fdf47335ec8554887b177b537f5850cb9b5023c
TamaraCuric <tamara.curic01@gmail.com>
2023-04-11 11:36:54 +0000

    chore: connected the database with script now it saves the games and results in database

--- number_guess.sh
+++ number_guess.sh
@@ -5,15 +5,17 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 echo "Enter your username:"
 read USERNAME
 
-# USERNAME_AVAIL=$($PSQL "SELECT username FROM users WHERE username='$USERNAME")
+USERNAME_AVAIL=$($PSQL "SELECT username FROM users WHERE username='$USERNAME'")
+GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM users INNER JOIN games USING(user_id) WHERE username='$USERNAME'")
+BEST_GAME=$($PSQL "SELECT MIN(number_guess) FROM users INNER JOIN games USING(user_id) WHERE username='$USERNAME'")
 
-# if [[ -z $USERNAME_AVAIL ]]
-#   then
-#     INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
-#     echo "Welcome, $USERNAME! It looks like this is your first time here."
-#     else
-#       echo "Welcome back, $USERNAME! You have played <games_played> games, and your best game took <best_game> guesses."
-# fi
+if [[ -z $USERNAME_AVAIL ]]
+  then
+    INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+    echo "Welcome, $USERNAME! It looks like this is your first time here."
+    else
+      echo "Welcome back, $USERNAME! You have played <games_played> games, and your best game took <best_game> guesses."
+fi
 
 RANDO_NUM=$((1 + $RANDOM % 1000))
 GUESS=1
@@ -46,3 +48,6 @@ if [[ $GUESS == 1 ]]
   else
     echo "You guessed it in $GUESS tries. The secret number was $RANDO_NUM. Nice job!"
 fi
+
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+INSERT_GAME=$($PSQL "INSERT INTO games(number_guess, user_id) VALUES($GUESS, $USER_ID)")
